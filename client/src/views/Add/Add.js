b221674dd501a7f6f4ad99509a36c9734c1c7885
Lucas Leite <lucas.a.r.leite@gmail.com>
2021-08-11 20:49:34 +0100

    Add view done

--- client/src/views/Add/Add.js
+++ client/src/views/Add/Add.js
@@ -2,10 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Button from '../../components/Button/Button';
+import Card from '../../components/Card/Card';
+
+import './Add.css';
 
 const Add = () => {
     return (
-        <div>
+        <div className="Add">
+            <Card>
+                <h3>Add a new quote</h3>
+                <form>
+                    <label for="quote" className="form-label">Quote</label>
+                    <textarea rows={3} className="form-control" id="quote" />
+                    <label for="author" className="form-label">Author</label>
+                    <input type="text" className="form-control" id="author" />
+                </form>
+            </Card>
+            <Button text="Add Quote" />
             <Link to="/">
                 <Button text="Back to Home" isFixed />
             </Link>
