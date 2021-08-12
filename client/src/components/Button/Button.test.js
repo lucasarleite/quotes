ff5521a8669a8bcf9d0ebc58646bf6f46611852a
Lucas Leite <lucas.a.r.leite@gmail.com>
2021-08-12 01:13:52 +0100

    tests setup, added tests to button component

---
+++ client/src/components/Button/Button.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Button from './Button';
+import { expect, test } from '@jest/globals';
+
+describe('Button Component Tests', () => {
+    test('it should render a Button', () => {
+        render(<Button />);
+        const btn = document.querySelectorAll('.Button');
+        expect(btn.length).toEqual(1);
+    });
+
+    test('it should render a Button with text', () => {
+        const text = 'test'
+        render(<Button text={text} />);
+        const btn = screen.getByText(text);
+        expect(btn).not.toBeUndefined();
+    });
+
+    test('it should render a fixed Button', () => {
+        render(<Button isFixed />);
+        const btn = document.querySelector('.Button');
+        expect(btn.classList.contains('fixed')).toEqual(true);
+    });
+
+    test('it should trigger onClick event', () => {
+        const mockClick = jest.fn();
+        render(<Button onClick={mockClick} />);
+        const btn = document.querySelector('.Button');
+        fireEvent.click(btn);
+        expect(mockClick).toHaveBeenCalled();
+    });
+});
